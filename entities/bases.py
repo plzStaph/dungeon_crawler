1803087b747f7cab5dfbcb6c9f575934bb1312f2
Daniel R <lom-pm-c@hotmail.com>
2020-06-28 16:48:09 -0500

    chore: fixed typos

--- entities/bases.py
+++ entities/bases.py
@@ -3,7 +3,7 @@ from utils.texture import Texture
 
 class Base(Textured):
     textures = {
-        "light"  : Texture("base/dark.png"),
+        "light"  : Texture("base/light.png"),
         "medium" : Texture("base/medium.png"),
-        "hard"   : Texture("base/hard.png")
+        "dark"   : Texture("base/dark.png")
     }
